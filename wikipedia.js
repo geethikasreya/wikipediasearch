1c4ca7f035fec2207c9f68ac148578fa27a2526a
Geethika sreya <135309107+geethikasreya@users.noreply.github.com>
2023-07-03 19:51:50 +0530

    Add files via upload

---
+++ wikipedia.js
@@ -0,0 +1,73 @@
+let searchInputEl = document.getElementById("searchInput");
+let searchResultsEl = document.getElementById("searchResults");
+let spinnerEl = document.getElementById("spinner");
+
+function createAndAppendSearchResult(result) {
+    let resultItemEL = document.createElement("div");
+    resultItemEL.classList.add("result-item");
+    searchResultsEl.appendChild(resultItemEL);
+
+
+    let {
+        link,
+        title,
+        description
+    } = result;
+    let titleEl = document.createElement("a");
+    titleEl.href = link;
+    titleEl.target = "_blank";
+    titleEl.textContent = title;
+    titleEl.classList.add("result-title");
+    resultItemEL.appendChild(titleEl);
+
+
+    let titleBreakEL = document.createElement("br");
+    resultItemEL.appendChild(titleBreakEL);
+
+    let urlEl = document.createElement("a");
+    urlEl.classList.add("result-url");
+    urlEl.href = link;
+    urlEl.target = "_blank";
+    urlEl.textContent = link;
+    resultItemEL.appendChild(urlEl);
+
+    let urlBreakEL = document.createElement("br");
+    resultItemEL.appendChild(urlBreakEL);
+
+
+    let descriptionEl = document.createElement("p");
+    descriptionEl.classList.add("link-description");
+    descriptionEl.textContent = description;
+    resultItemEL.appendChild(descriptionEl);
+}
+
+function displayResults(searchResults) {
+    spinnerEl.classList.toggle("d-none");
+    for (let result of searchResults) {
+        createAndAppendSearchResult(result);
+    }
+}
+
+function searchWikipedia(event) {
+    if (event.key === "Enter") {
+        spinnerEl.classList.toggle("d-none");
+        searchResultsEl.textContent = "";
+        let searchInputValue = searchInputEl.value;
+        let url = "https://apis.ccbp.in/wiki-search?search=" + searchInputValue;
+        let options = {
+            method: "GET"
+        };
+        fetch(url, options)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(jsonData) {
+                let {
+                    search_results
+                } = jsonData;
+                displayResults(search_results);
+            });
+    }
+
+}
+searchInputEl.addEventListener("keydown", searchWikipedia);
